5834dcffa78f482af8d66392b0513262255b3b4c
Gamze Gülyol <ggulyol@gmail.com>
2018-04-08 15:17:14 +0300

    Add files via upload

---
+++ dbconfig.php
@@ -0,0 +1,11 @@
+<?php
+      
+$server     = 'localhost';
+$username   = 'root';
+$password   = 'gamzegulyol';
+$database   = 'eczaneler';
+  
+$dsn        = "mysql:host=$server;dbname=$database";
+
+
+?>
